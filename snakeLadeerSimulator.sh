1e8b1f76c4d69957a7a58ed1a6e1a8fa8b9a87ad
priyankagarad <garadpriyanka1995@gmail.com>
2020-03-25 14:41:39 +0530

    Added:calculated positiion of player still  it reaches 100 position

--- snakeLadeerSimulator.sh
+++ snakeLadeerSimulator.sh
@@ -1,29 +1,43 @@
 #!/bin/bash -x
 echo "Welcome snake ladder simulator"
+
 #CONSTANT
 PLAYER1=0
 NO_PLAY=0
 LADDER=1
 SNAKE=2
+WINNING_POSITION=100
 
 #variable
 start_position=0
 position=0
 
-#die rolls get number between 1 to 6
-die_roll=$((RANDOM%6))
-
 #check option for no_player,ladder,snake
-read -p "enter a choice" choice
-case $choice in
-		$NO_PLAY)
-			start_position=$start_position
-			;;
-		$LADDER)
-			start_position=$(($start_postion+$die_roll))
-			;;
-		$SNAKE)
-			start_position=$(($start_position-$die_roll))
-			;;
-esac
-echo $start_position
+function playing_game()
+{
+	while [ $position -lt $WINNING_POSITION ]
+	do
+		die_roll=$((RANDOM%6))
+		random_number=$((RANDOM%3))
+		case $random_number in
+			$NO_PLAY)
+				start_position=$start_position
+				;;
+			$LADDER)
+				start_position=$(($start_postion+$die_roll))
+				;;
+			$SNAKE)
+				start_position=$(($start_position-$die_roll))
+				;;
+		esac
+		echo $start_position
+		((position++))
+	done
+}
+playing_game
+
+#if position less than zero than it call function
+if [ $position -lt 0 ]
+then
+	playing_game
+fi
